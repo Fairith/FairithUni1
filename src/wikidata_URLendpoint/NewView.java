13a0143e120f5692d8d28379ac3a9b686255ca30
Fairith <feboe1990@gmx.net>
2018-01-31 19:54:18 +0100

    more swing GUI

--- src/wikidata_URLendpoint/NewView.java
+++ src/wikidata_URLendpoint/NewView.java
@@ -9,10 +9,23 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.DefaultComboBoxModel;
 
 public class NewView extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txf_input;
+	private JTextArea txa_output;
+	
+	private Model model;
 
 	/**
 	 * Launch the application.
@@ -41,22 +54,73 @@ public class NewView extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JLabel lblEnterATerm = new JLabel("Enter a Term to translate it!");
+		JLabel lblEnterATerm = new JLabel("Enter a Term to translate!");
+		
+		txf_input = new JTextField();
+		txf_input.setColumns(10);
+		
+		JComboBox comboBox_originLanguage = new JComboBox();
+		comboBox_originLanguage.setModel(new DefaultComboBoxModel(new String[] {"DE", "EN", "FR"}));
+		
+		JButton btn_translate = new JButton("Translate");
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Results", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(404)
-					.addComponent(lblEnterATerm, GroupLayout.DEFAULT_SIZE, 222, Short.MAX_VALUE)
+					.addComponent(lblEnterATerm, GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE)
 					.addGap(336))
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addComponent(comboBox_originLanguage, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(txf_input, GroupLayout.DEFAULT_SIZE, 826, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addComponent(btn_translate)
+					.addContainerGap())
+				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 864, Short.MAX_VALUE)
+					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblEnterATerm)
-					.addContainerGap(527, Short.MAX_VALUE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(txf_input, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(comboBox_originLanguage, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addComponent(btn_translate)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 426, Short.MAX_VALUE)
+					.addContainerGap())
 		);
+		
+		JComboBox comboBox_targetLanguage = new JComboBox();
+		comboBox_targetLanguage.setModel(new DefaultComboBoxModel(new String[] {"DE", "EN", "FR"}));
+		
+		txa_output = new JTextArea();
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(comboBox_targetLanguage, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addComponent(txa_output, GroupLayout.PREFERRED_SIZE, 820, GroupLayout.PREFERRED_SIZE))
+		);
+		gl_panel.setVerticalGroup(
+			gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addComponent(comboBox_targetLanguage, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(383, Short.MAX_VALUE))
+				.addComponent(txa_output, GroupLayout.DEFAULT_SIZE, 403, Short.MAX_VALUE)
+		);
+		panel.setLayout(gl_panel);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
